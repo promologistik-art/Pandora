4a62dfaca524973e3205badc14268d7bfded691d
promologistik-art <promologistik@gmail.com>
2026-07-22 15:14:23 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -20,9 +20,8 @@ class Config:
 
     # === 3x-ui API ===
     XUI_HOST: str = os.getenv("XUI_HOST", "")
-    XUI_USERNAME: str = os.getenv("XUI_USERNAME", "")
-    XUI_PASSWORD: str = os.getenv("XUI_PASSWORD", "")
     XUI_INBOUND_ID: int = int(os.getenv("XUI_INBOUND_ID", "1"))
+    XUI_API_TOKEN: str = os.getenv("XUI_API_TOKEN", "")
 
     # === СБП ===
     SBP_PHONE: str = os.getenv("SBP_PHONE", "+79991234567")
@@ -30,7 +29,7 @@ class Config:
 
     # === Внешние ссылки ===
     SUPPORT_BOT_USERNAME: str = os.getenv("SUPPORT_BOT_USERNAME", "silverzen_bot")
-    VK_PAGE: str = os.getenv("VK_PAGE", "https://vk.com/your_closed_page")
+    VK_PAGE: str = os.getenv("VK_PAGE", "https://vk.ru/pyxispandorae")
 
     # === Тарифы ===
     TRIAL_DAYS: int = 3
